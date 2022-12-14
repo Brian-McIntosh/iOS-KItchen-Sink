387669063b1352f2f648c4fe70c74478c96fa8c5
BMCINT20 <bmcint20@ford.com>
2022-12-14 11:09:56 -0500

    Initial Commit

---
+++ iOS KItchen Sink/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS KItchen Sink
+//
+//  Created by Brian McIntosh on 12/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
